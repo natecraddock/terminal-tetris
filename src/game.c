2252616a82c633e121772d86c27b52a99b701595
Nathan Craddock <nzcraddock@gmail.com>
2021-04-24 15:55:17 -0600

    Types for pieces and points
    Structs for pieces and points and util functions for each.

--- src/game.c
+++ src/game.c
@@ -12,7 +12,78 @@ typedef enum {
     PIECE_J,
     PIECE_S,
     PIECE_Z,
-} Pieces;
+} PieceType;
+
+typedef struct {
+    int x;
+    int y;
+} Point;
+
+typedef struct {
+    Point location;
+    Point blocks[4];
+    PieceType type;
+} Piece;
+
+static void point_set(Point *point, int x, int y){
+    point->x = x;
+    point->y = y;
+}
+
+static void piece_new(Piece *piece, PieceType type){
+    piece->type = type;
+
+    switch (type){
+        case PIECE_O:
+            point_set(&piece->blocks[0], 1, 0);
+            point_set(&piece->blocks[1], 2, 0);
+            point_set(&piece->blocks[2], 1, 1);
+            point_set(&piece->blocks[3], 2, 1);
+            break;
+
+        case PIECE_I:
+            point_set(&piece->blocks[0], 1, 0);
+            point_set(&piece->blocks[1], 1, 1);
+            point_set(&piece->blocks[2], 1, 2);
+            point_set(&piece->blocks[3], 1, 3);
+            break;
+
+        case PIECE_T:
+            point_set(&piece->blocks[0], 1, 0);
+            point_set(&piece->blocks[1], 0, 1);
+            point_set(&piece->blocks[2], 1, 1);
+            point_set(&piece->blocks[3], 2, 1);
+            break;
+
+        case PIECE_L:
+            point_set(&piece->blocks[0], 2, 0);
+            point_set(&piece->blocks[1], 0, 1);
+            point_set(&piece->blocks[2], 1, 1);
+            point_set(&piece->blocks[3], 2, 1);
+            break;
+
+        case PIECE_J:
+            point_set(&piece->blocks[0], 0, 0);
+            point_set(&piece->blocks[1], 0, 1);
+            point_set(&piece->blocks[2], 1, 1);
+            point_set(&piece->blocks[3], 2, 1);
+            break;
+
+        case PIECE_S:
+            point_set(&piece->blocks[0], 1, 0);
+            point_set(&piece->blocks[1], 2, 0);
+            point_set(&piece->blocks[2], 0, 1);
+            point_set(&piece->blocks[3], 1, 1);
+            break;
+
+        case PIECE_Z:
+            point_set(&piece->blocks[0], 0, 0);
+            point_set(&piece->blocks[1], 1, 0);
+            point_set(&piece->blocks[2], 1, 1);
+            point_set(&piece->blocks[3], 1, 2);
+            break;
+    }
+}
 
 static void handle_menu_event(Game *game, Event event) {
     if (event == EVENT_SELECT) {
